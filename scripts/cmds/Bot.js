30641f8a724e3cb2298d7d7078fdcba9257f649e
Efat27 <mdifad381@gmail.com>
2025-01-10 02:35:35 +0600

    Update Bot.js

--- scripts/cmds/Bot.js
+++ scripts/cmds/Bot.js
@@ -1,25 +1,3 @@
-module.exports = {
-  config: {
-    name: "bot",
-    version: "1.0",
-    author: "Efat Ahmed",
-    description: "Responds to 'bot' and 'bby'",
-  },
-  onChat: async function ({ message, event, api }) {
-    const keywords = ["bot", "bby"];
-    const responses = {
-      bot: "Hi, I'm here! How can I assist you? 🤖",
-      bby: "Hello there! What's up? 🥺",
-    };
-
-    if (!event.body) return;
-
-    const userInput = event.body.toLowerCase();
-    const matchedKeyword = keywords.find((keyword) => userInput.includes(keyword));
-
-    if (matchedKeyword) {
-      const response = responses[matchedKeyword];
-      return message.reply({ body: response });
-    }
-  },
-};
+function hi() {
+  console.log("Hello World!");
+}
